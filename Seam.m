1ed059db816317807c0d0671098f3339fb4203a7
Shivang Srivastava <satyamsri8@users.noreply.github.com>
2018-01-13 09:50:18 +0530

    Applying Gradient Magnitude
    Using imgradient function by Prewitt Method

--- Seam.m
+++ Seam.m
@@ -1,10 +1,18 @@
 clear
 
 load('C:\Users\acer\Desktop\Image Processing\Seam-Carving\Tower.mat')
+
+%imshow(Tower)
+%{
 figure;
 level = graythresh(Tower);
 img = im2bw(Tower,level);
-imshow(img)
-figure;
-img = im2bw(Tower);
-imshow(img)
+%}
+I = rgb2gray(Tower);
+%imshow(I)
+%[Gmag,Gdir] = imgradient(I);
+[Gmag, Gdir] = imgradient(I,'prewitt');
+
+figure
+imshowpair(Gmag, Gdir, 'montage');
+title('Gradient Magnitude, Gmag (left), and Gradient Direction, Gdir (right), using Prewitt method')
