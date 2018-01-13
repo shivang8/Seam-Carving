c3a379f0b6899d61fb9431e22bd3a56064ced762
Shivang Srivastava <satyamsri8@users.noreply.github.com>
2018-01-13 17:12:10 +0530

    Added DP

--- Seam.m
+++ Seam.m
@@ -3,16 +3,30 @@ clear
 load('C:\Users\acer\Desktop\Image Processing\Seam-Carving\Tower.mat')
 
 %imshow(Tower)
+
+I = rgb2gray(Tower); % Greyscale Conversion
+
+%figure;
+%imshow(I)
+[Gmag, Gdir] = imgradient(I,'prewitt'); % Gradient Method
 %{
 figure;
-level = graythresh(Tower);
-img = im2bw(Tower,level);
-%}
-I = rgb2gray(Tower);
-%imshow(I)
-%[Gmag,Gdir] = imgradient(I);
-[Gmag, Gdir] = imgradient(I,'prewitt');
+imshow(Gmag)
 
-figure
+figure;
 imshowpair(Gmag, Gdir, 'montage');
 title('Gradient Magnitude, Gmag (left), and Gradient Direction, Gdir (right), using Prewitt method')
+%}
+dp = Gmag;
+sz = size(dp);
+for i = 2:sz(1)
+    for j = 1:sz(2)
+        if j == 1
+            dp(i,j) = dp(i,j) + min(dp(i-1,j),dp(i-1,j+1));
+        elseif j == sz(2)
+            dp(i,j) = dp(i,j) + min(dp(i-1,j-1),dp(i-1,j));
+        else
+            dp(i,j) = dp(i,j) + min(dp(i-1,j-1),min(dp(i-1,j),dp(i-1,j+1)));
+        end
+    end
+end
